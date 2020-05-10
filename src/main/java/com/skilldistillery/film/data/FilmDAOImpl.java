ab508bbe82df058cfe3e16f1ec7bb1fa37fc5474
Sergei Samoiloff <sergio.samoiloff@gmail.com>
2020-05-09 18:43:19 -0600

    throwaway

--- src/main/java/com/skilldistillery/film/data/FilmDAOImpl.java
+++ src/main/java/com/skilldistillery/film/data/FilmDAOImpl.java
@@ -57,8 +57,16 @@ public class FilmDAOImpl implements FilmDAO {
 
 	@Override
 	public Film createFilm(Film film) {
-		// TODO Auto-generated method stub
-		return null;
+		Film film = null;
+		String sql = "INSERT INTO film (title, description, release_year, rental_duration, rental_rate, length, "
+				+ "replacement_cost, )WHERE film.id = ?";
+		Connection conn = null;
+		try {
+			conn = DriverManager.getConnection(url, user, pass);
+			PreparedStatement stmt = conn.prepareStatement(sql);
+			stmt.setInt(1, filmId);
+			ResultSet rs = stmt.executeQuery();
+		return film;
 	}
 
 	@Override
