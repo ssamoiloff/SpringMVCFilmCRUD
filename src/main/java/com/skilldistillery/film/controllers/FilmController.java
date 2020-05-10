818dba1dc4355158c3be4b87db6e23f67dc0636e
Shane Duncan <aequitas321@icloud.com>
2020-05-09 21:04:37 -0400

    edited controller for creating a film object

--- src/main/java/com/skilldistillery/film/controllers/FilmController.java
+++ src/main/java/com/skilldistillery/film/controllers/FilmController.java
@@ -5,8 +5,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-import com.skilldistillery.film.entities.*;
-import com.skilldistillery.film.data.*;
+
+import com.skilldistillery.film.data.FilmDAO;
+import com.skilldistillery.film.entities.Film;
 @Controller
 public class FilmController {
     @Autowired
@@ -34,51 +35,21 @@ public class FilmController {
         mv.setViewName("WEB-INF/views/searchById.jsp");
         return mv;
     }
-//  @RequestMapping(path = "GetStateData.do", params = "abbr", method = RequestMethod.GET)
-//  public ModelAndView getStateByAbbreviation(String abbr) {
-//      ModelAndView mv = new ModelAndView();
-//      State s = stateDAO.getStateByAbbreviation(abbr);
-//      mv.addObject("state", s);
-//      mv.setViewName("result");
-//      return mv;
-//  }
-//
-//  @RequestMapping(path = "NewState.do", method = RequestMethod.POST)
-//  public ModelAndView newState(State state) {
-//      stateDAO.addState(state);
-//      ModelAndView mv = new ModelAndView();
-//      mv.setViewName("result");
-//      return mv;
-//  }
-    // TODO : Implement another request handler for:
-    // path "NewState.do"
-    // method POST
-    // command object : State
-    // return : ModelAndView
-    // view : "redirect:stateAdded.do"
-    // behavior : add state to dao, add state to flashAttributes
-    // Note: Comment out the other "NewState.do" request handler method
-//  @RequestMapping(path = "NewState.do", method = RequestMethod.POST)
-//  public String newState(State state, RedirectAttributes redir) {
-//      stateDAO.addState(state);
-//      redir.addFlashAttribute("state", state);
-//      return "redirect:stateAdded.do";
-//  }
-    // TODO : Implement a request handler for:
-    // path "stateAdded.do"
-    // method GET
-    // command object : State
-    // return : ModelAndView
-    // view : "WEB-INF/result.jsp", "WEB-INF/result", or "result" if using
-    // InternalResourceViewResolver
-    // Note: fix other request handler methods to use InternalResourceViewResolver
-//  @RequestMapping("stateAdded.do")
-//  public ModelAndView stateAdded() {
-//      ModelAndView mv = new ModelAndView();
-//      // This uses InternalResourceViewResolver with WEB-INF and .jsp as the prefix
-//      // and suffix
-//      mv.setViewName("result");
-//      return mv;
-//  }
+    
+    @RequestMapping(path="createFilm.do", method = RequestMethod.POST)
+    public ModelAndView createFilm(Film film, RedirectAttributes redir) {
+    	filmDAO.createFilm(film);
+    	ModelAndView mv = new ModelAndView();
+    	redir.addFlashAttribute("film", film);
+    	mv.setViewName("redirect:filmCreated.do");
+    	return mv;
+    }
+    
+    @RequestMapping(path="filmCreated.do", method = RequestMethod.GET)
+    public ModelAndView created() {
+    	ModelAndView mv = new ModelAndView();
+    	mv.setViewName("WEB-INF/views/createFilm.jsp");
+    	return mv;
+    }
 }
 
